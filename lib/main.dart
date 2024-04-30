f35eb272322d79afa5981b0662abd5fba572035c
zobayerdev <zobayer.dev@gmail.com>
2024-05-01 00:08:33 +0600

    web version is uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:ostad_livetest_two/profile_section.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        tools: const [
+          ...DevicePreview.defaultTools,
+        ],
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
